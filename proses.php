0a4f8fd40110cf056f0f8ca693e326f83e2cd7d0
DidaPradana <42725223+didapradana@users.noreply.github.com>
2018-09-10 09:55:24 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,42 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+	
+$angka=$_GET['angka'];
+
+// $angka = 5;
+function pertama($x, $y){
+	if ($x > 0) {
+		if ($y > 0) {
+			echo "* ";
+			$y--;
+			pertama($x, $y);
+		}
+		else{
+			echo "<br>";
+			$x--;
+			pertama($x, $x);
+		}
+	}
+}
+pertama ($angka, $angka);
+
+echo "<br> <br>";
+
+$bilangan = $angka;
+$total1 = 0;
+$total2 = 0;
+$bil_awal = 1;
+    while ($bil_awal <= $bilangan) {
+        if ($bil_awal % 2 == 0) {
+            echo "Bilangan genap = " . $bil_awal . "<br/>";
+                $bil_awal++;
+                $total1 = $total1+1;
+        } else {
+          	echo "Bilangan ganjil = " . $bil_awal . "<br/>";
+                $bil_awal++;
+                $total2 = $total2+1;
+                }
+        }
+        echo "Jumlah Deret Genap = $total1 <br>";
+        echo "Jumlah Deret Ganjil = $total2" ;
 
 ?>
